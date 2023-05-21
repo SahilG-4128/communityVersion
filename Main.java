d40386b8e92ffc4b84b4f6aa473ad94380dbde88
Sahil Goel <goelsahil4128@gmail.com>
2023-05-21 11:12:34 +0530

    My community version first commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello first program");
+        int a=3,b=9;
+        System.out.println("Sum is:"+(a+b));
+    }
+}
