9721fa6884dbc5e8f408934bb37bea00c2a6bcdc
Sahil Goel <goelsahil4128@gmail.com>
2023-05-21 11:47:32 +0530

    Difference file is getting added

---
+++ Difference.java
@@ -0,0 +1,7 @@
+public class Difference {
+    public static void main(String[] args) {
+        System.out.println("Difference file");
+        int a=99,b=88;
+        System.out.println("Difference is:"+(a-b));
+    }
+}
