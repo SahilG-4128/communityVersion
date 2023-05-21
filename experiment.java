85b5dd0ea8e934dfea91e5ae7c0a6f6f18c65625
Sahil Goel <goelsahil4128@gmail.com>
2023-05-21 11:27:03 +0530

    my third experiment commit

---
+++ experiment.java
@@ -0,0 +1,7 @@
+public class experiment {
+    public static void main(String[] args) {
+        System.out.println("This is just an experiment");
+        int a=9,b=8,c55;
+        System.out.println("Answer is:"+(a*(b+c)));
+    }
+}
